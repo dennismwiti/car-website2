f8ddc2a319259ca6a440eafb222d62e67950615e
dennismwiti <blockbuster045@gmail.com>
2023-11-10 11:40:40 +0300

    update settings.py host added to allowed hosts

--- carseller/settings.py
+++ carseller/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['misspowershow.onrender.com']
+ALLOWED_HOSTS = ['misspowershow.onrender.com', '127.0.0.1']
 
 SITE_ID = 1
 
