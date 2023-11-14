18a32e1cc431f9fed53fe70d19c848215d31593a
dennismwiti <blockbuster045@gmail.com>
2023-11-14 13:06:27 +0300

    update settings.py(MEDIA_ROOT)

--- carseller/settings.py
+++ carseller/settings.py
@@ -179,8 +179,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # MEDIA_ROOT = '/Desktop/PythonProjects/car-website2/media'
 
 # STATICFILES_DIRS = [
