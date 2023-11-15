fabd6bcb313595f540f54952e1c4ce245ca84f1d
dennismwiti <blockbuster045@gmail.com>
2023-11-15 17:26:04 +0300

    update config.py

--- carseller/config.py
+++ carseller/config.py
@@ -5,4 +5,3 @@ EMAIL_HOST_PASSWORD = 'avxqlyzlozikcyay'
 
 SECRET_KEY = '2ea37aca1fccdc5c6f6c1a83d4fe4bdd83e3aae70afbeabac6fb800908dc23bc'
 
-GIT_PASSWORD = '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'
