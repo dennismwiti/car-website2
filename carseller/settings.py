37c16fa396fda6547041d917c56e9cb9d95299b9
dennismwiti <blockbuster045@gmail.com>
2023-11-28 15:50:09 +0300

    update setting.py(d=false)

--- carseller/settings.py
+++ carseller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['misspowershow.onrender.com', '127.0.0.1']
 
